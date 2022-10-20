01f44277b5bf67b2bcd2837cdbbebb3cd7574dff
nozzi325 <nozzia3@gmail.com>
2022-10-20 15:37:09 +0300

    Added ModelMapper bean to Spring Context

--- src/main/java/com/andrew/FinancialHelper/FinancialHelperApplication.java
+++ src/main/java/com/andrew/FinancialHelper/FinancialHelperApplication.java
@@ -1,11 +1,16 @@
 package com.andrew.FinancialHelper;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FinancialHelperApplication {
-
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(FinancialHelperApplication.class, args);
 	}
