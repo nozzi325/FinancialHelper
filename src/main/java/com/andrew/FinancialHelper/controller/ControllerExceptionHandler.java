30a6a574c01962b09e85eae5db5421862145b2f7
nozzi325 <nozzia3@gmail.com>
2022-10-20 15:55:19 +0300

    Created ControllerExceptionHandler class and util ErrorDetails class to display errors

---
+++ src/main/java/com/andrew/FinancialHelper/controller/ControllerExceptionHandler.java
@@ -0,0 +1,54 @@
+package com.andrew.FinancialHelper.controller;
+
+
+import com.andrew.FinancialHelper.exception.*;
+import com.andrew.FinancialHelper.utils.ErrorDetails;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ControllerExceptionHandler {
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ErrorDetails> exceptionNotFoundHandler(NotFoundException e) {
+        ErrorDetails errorDetails = new ErrorDetails();
+        errorDetails.setMessage(e.getMessage());
+        return ResponseEntity
+                .badRequest()
+                .body(errorDetails);
+    }
+
+    @ExceptionHandler(InsufficientFundsException.class)
+    public ResponseEntity<ErrorDetails> exceptionInsufficientFundsHandler(InsufficientFundsException e) {
+        ErrorDetails errorDetails = new ErrorDetails();
+        errorDetails.setMessage(e.getMessage());
+        return ResponseEntity
+                .badRequest()
+                .body(errorDetails);
+    }
+
+    @ExceptionHandler(UserEmailAlreadyTakenException.class)
+    public ResponseEntity<ErrorDetails> exceptionUserEmailAlreadyTakenHandler(UserEmailAlreadyTakenException e) {
+        ErrorDetails errorDetails = new ErrorDetails();
+        errorDetails.setMessage(e.getMessage());
+        return ResponseEntity
+                .badRequest()
+                .body(errorDetails);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorDetails> exceptionMethodArgumentNotValidHandler(MethodArgumentNotValidException e) {
+        ErrorDetails errorDetails = new ErrorDetails();
+        StringBuilder errMessage = new StringBuilder();
+        for (FieldError fieldError : e.getBindingResult().getFieldErrors()) {
+            errMessage.append(String.format("%s; ",fieldError.getDefaultMessage()));
+        }
+        errorDetails.setMessage(String.valueOf(errMessage).trim());
+        return ResponseEntity
+                .badRequest()
+                .body(errorDetails);
+    }
+}
---
+++ src/main/java/com/andrew/FinancialHelper/utils/ErrorDetails.java
@@ -0,0 +1,12 @@
+package com.andrew.FinancialHelper.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorDetails {
+    String message;
+}
