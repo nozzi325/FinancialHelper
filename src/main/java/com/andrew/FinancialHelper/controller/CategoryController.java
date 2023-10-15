b30fd6c31e4633b5ce09d29234cca01093bfc66d
nozzi325 <nozzia3@gmail.com>
2023-10-15 14:35:09 +0300

    Refactored Category related classes

--- src/main/java/com/andrew/FinancialHelper/controller/CategoryController.java
+++ src/main/java/com/andrew/FinancialHelper/controller/CategoryController.java
@@ -4,59 +4,73 @@ import com.andrew.FinancialHelper.db.entity.Category;
 import com.andrew.FinancialHelper.dto.request.CategoryRequest;
 import com.andrew.FinancialHelper.dto.response.CategoryResponse;
 import com.andrew.FinancialHelper.service.CategoryService;
-
-import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@AllArgsConstructor
 @RequestMapping("/api/categories")
 public class CategoryController {
     private final CategoryService categoryService;
     private final ModelMapper modelMapper;
 
+    public CategoryController(CategoryService categoryService, ModelMapper modelMapper) {
+        this.categoryService = categoryService;
+        this.modelMapper = modelMapper;
+    }
+
     @GetMapping
-    public List<CategoryResponse> getCategories(){
+    public List<CategoryResponse> getAllCategories() {
         return categoryService.getAllCategories()
                 .stream()
                 .map(this::convertToResponse)
                 .toList();
     }
 
-    @GetMapping("{id}")
-    public CategoryResponse getCategoryById(@PathVariable Long id){
+    @GetMapping("/{id}")
+    public CategoryResponse getCategoryById(@PathVariable Long id) {
         return convertToResponse(categoryService.findCategoryById(id));
     }
 
     @PostMapping
-    public ResponseEntity<HttpStatus> createCategory(@RequestBody @Valid CategoryRequest request){
-        categoryService.createCategory(convertToEntity(request));
-        return ResponseEntity.ok(HttpStatus.CREATED);
+    public ResponseEntity<CategoryResponse> createCategory(@RequestBody @Valid CategoryRequest request) {
+        Category category = categoryService.updateCategory(convertToEntity(request));
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(convertToResponse(category));
     }
 
-    @PutMapping
-    public ResponseEntity<HttpStatus> updateCategory(@RequestBody @Valid CategoryRequest request){
-        categoryService.updateCategory(convertToEntity(request));
-        return ResponseEntity.ok(HttpStatus.NO_CONTENT);
+    @PutMapping("/{id}")
+    public ResponseEntity<CategoryResponse> updateCategory(@RequestBody @Valid CategoryRequest request) {
+        Category category = categoryService.updateCategory(convertToEntity(request));
+        return ResponseEntity
+                .ok()
+                .body(convertToResponse(category));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<HttpStatus> deleteCategory(@PathVariable Long id){
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCategory(@PathVariable Long id) {
         categoryService.deleteCategory(id);
-        return ResponseEntity.ok(HttpStatus.NO_CONTENT);
     }
 
     private Category convertToEntity(CategoryRequest request) {
-        return modelMapper.map(request,Category.class);
+        return modelMapper.map(request, Category.class);
     }
 
     private CategoryResponse convertToResponse(Category category) {
-        return modelMapper.map(category,CategoryResponse.class);
+        return modelMapper.map(category, CategoryResponse.class);
     }
 }
--- src/main/java/com/andrew/FinancialHelper/service/CategoryService.java
+++ src/main/java/com/andrew/FinancialHelper/service/CategoryService.java
@@ -3,45 +3,46 @@ package com.andrew.FinancialHelper.service;
 
 import com.andrew.FinancialHelper.db.entity.Category;
 import com.andrew.FinancialHelper.db.repository.CategoryRepository;
-import com.andrew.FinancialHelper.exception.CategoryNotFoundException;
-import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
-@AllArgsConstructor
 @Service
 public class CategoryService {
     private final CategoryRepository categoryRepository;
 
-    public Category findCategoryById(Long id){
-        return categoryRepository.findById(id)
-                .orElseThrow(() -> new CategoryNotFoundException(String.format("Category with id %d not found",id)));
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
     }
 
-    public List<Category> getAllCategories(){
+    public Category findCategoryById(Long id) {
+        return categoryRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(String.format("Category with id %d not found", id)));
+    }
+
+    public List<Category> getAllCategories() {
         return categoryRepository.findAll();
     }
 
     @Transactional
-    public void createCategory(Category category){
-        categoryRepository.save(category);
+    public Category createCategory(Category category) {
+        return categoryRepository.save(category);
     }
 
     @Transactional
-    public void deleteCategory(Long id){
-        if (!categoryRepository.existsById(id)){
-            throw new CategoryNotFoundException(String.format("Category with id %d not found", id));
+    public void deleteCategory(Long id) {
+        if (!categoryRepository.existsById(id)) {
+            throw new EntityNotFoundException(String.format("Category with id %d not found", id));
         }
         categoryRepository.deleteById(id);
     }
 
     @Transactional
-    public void updateCategory(Category category){
-        if (!categoryRepository.existsById(category.getId())){
-            throw new CategoryNotFoundException(String.format("Category with id %d not found", category.getId()));
-        }
-        categoryRepository.save(category);
+    public Category updateCategory(Category updatedCategory) {
+        Category existingCategory = findCategoryById(updatedCategory.getId());
+        existingCategory.setName(updatedCategory.getName());
+        return categoryRepository.save(existingCategory);
     }
 }
