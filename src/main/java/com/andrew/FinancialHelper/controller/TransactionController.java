024643efaae41bb7b74157597d71cd7ec98d81f9
nozzi325 <nozzia3@gmail.com>
2023-10-15 16:38:07 +0300

    Refactored Transaction related classes

--- src/main/java/com/andrew/FinancialHelper/controller/TransactionController.java
+++ src/main/java/com/andrew/FinancialHelper/controller/TransactionController.java
@@ -1,91 +1,96 @@
 package com.andrew.FinancialHelper.controller;
 
+import com.andrew.FinancialHelper.db.entity.Transaction;
 import com.andrew.FinancialHelper.dto.request.TransactionRequest;
 import com.andrew.FinancialHelper.dto.response.TransactionResponse;
-import com.andrew.FinancialHelper.db.entity.Account;
-import com.andrew.FinancialHelper.db.entity.Category;
-import com.andrew.FinancialHelper.db.entity.Transaction;
-import com.andrew.FinancialHelper.service.AccountService;
-import com.andrew.FinancialHelper.service.CategoryService;
 import com.andrew.FinancialHelper.service.TransactionService;
-
-import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
 import java.time.LocalDate;
 import java.util.List;
 
-@AllArgsConstructor
 @RestController
 @RequestMapping("/api/transactions")
 public class TransactionController {
     private final TransactionService transactionService;
-    private final AccountService accountService;
-    private final CategoryService categoryService;
     private final ModelMapper modelMapper;
 
+    public TransactionController(TransactionService transactionService, ModelMapper modelMapper) {
+        this.transactionService = transactionService;
+        this.modelMapper = modelMapper;
+    }
+
     @GetMapping()
-    public List<TransactionResponse> getAllTransactions(){
-        return transactionService.getTransactions().stream()
+    public List<TransactionResponse> getAllTransactions() {
+        List<Transaction> transactions = transactionService.getTransactions();
+        return transactions.stream()
                 .map(this::convertToResponse)
                 .toList();
     }
 
     @GetMapping("{id}")
-    public TransactionResponse getTransactionById(@PathVariable Long id){
-        return convertToResponse(transactionService.getTransaction(id));
+    public TransactionResponse getTransactionById(@PathVariable Long id) {
+        Transaction transaction = transactionService.getTransactionById(id);
+        return convertToResponse(transaction);
     }
 
     @GetMapping(params = {"start", "end"})
     public List<TransactionResponse> getTransactionsByPeriod(
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate start,
-            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate end)
-    {
-        return transactionService.getTransactionsByPeriod(start, end).stream()
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate end) {
+        List<Transaction> transactions = transactionService.getTransactionsByPeriod(start, end);
+        return transactions.stream()
                 .map(this::convertToResponse)
                 .toList();
     }
 
     @GetMapping(params = "categoryId")
-    public List<TransactionResponse> getTransactionsByCategory(@RequestParam("categoryId") Long id){
-        return transactionService.getTransactionsByCategoryId(id).stream()
+    public List<TransactionResponse> getTransactionsByCategory(@RequestParam("categoryId") Long id) {
+        List<Transaction> transactions = transactionService.getTransactionsByCategoryId(id);
+        return transactions.stream()
                 .map(this::convertToResponse)
                 .toList();
     }
+
     @GetMapping(params = "accountId")
-    public List<TransactionResponse> getTransactionsByAccountId(@RequestParam("accountId") Long id){
-        return transactionService.getTransactionsByAccountId(id).stream()
+    public List<TransactionResponse> getTransactionsByAccountId(@RequestParam("accountId") Long id) {
+        List<Transaction> transactions = transactionService.getTransactionsByAccountId(id);
+        return transactions.stream()
                 .map(this::convertToResponse)
                 .toList();
     }
 
     @PostMapping
-    public ResponseEntity<HttpStatus> createTransaction(@RequestBody TransactionRequest transactionRequest){
-        Transaction transaction = convertToEntity(transactionRequest);
-        Account account = accountService.findAccountById(transactionRequest.getAccountId());
-        transaction.setAccount(account);
-        Category category = categoryService.findCategoryById(transactionRequest.getCategoryId());
-        transaction.setCategory(category);
-        transactionService.createTransaction(transaction);
-        return ResponseEntity.ok(HttpStatus.CREATED);
+    public ResponseEntity<TransactionResponse> createTransaction(@RequestBody TransactionRequest transactionRequest) {
+        Transaction transaction = transactionService.createTransaction(transactionRequest);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(convertToResponse(transaction));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<HttpStatus> deleteTransaction(@PathVariable Long id){
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteTransaction(@PathVariable Long id) {
         transactionService.deleteTransaction(id);
-        return ResponseEntity.ok(HttpStatus.OK);
     }
 
-    private TransactionResponse convertToResponse(Transaction transaction){
+    private TransactionResponse convertToResponse(Transaction transaction) {
         return modelMapper.map(transaction, TransactionResponse.class);
     }
 
-    private Transaction convertToEntity(TransactionRequest request){
-        return modelMapper.map(request,Transaction.class);
+    private Transaction convertToEntity(TransactionRequest request) {
+        return modelMapper.map(request, Transaction.class);
     }
 }
