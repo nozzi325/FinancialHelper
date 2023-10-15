42a3363bd0ce3b46a011ee6a445c0d9eb341bdf9
nozzi325 <nozzia3@gmail.com>
2023-10-15 17:23:09 +0300

    Updated ControllerExceptionHandler

--- src/main/java/com/andrew/FinancialHelper/controller/ControllerExceptionHandler.java
+++ src/main/java/com/andrew/FinancialHelper/controller/ControllerExceptionHandler.java
@@ -1,11 +1,9 @@
 package com.andrew.FinancialHelper.controller;
 
 
-import com.andrew.FinancialHelper.exception.InsufficientFundsException;
-import com.andrew.FinancialHelper.exception.NotFoundException;
-import com.andrew.FinancialHelper.exception.UserEmailAlreadyTakenException;
 import com.andrew.FinancialHelper.dto.ErrorDetails;
-
+import com.andrew.FinancialHelper.exception.InsufficientFundsException;
+import com.andrew.FinancialHelper.exception.UserEmailAlreadyTakenException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -13,6 +11,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.persistence.EntityNotFoundException;
 import java.time.LocalDateTime;
 import java.util.stream.Collectors;
 
@@ -21,7 +20,7 @@ public class ControllerExceptionHandler {
     // TODO: Review the project's exception system to ensure it's well-designed.
     //  After the review, assess whether additional methods or information are needed in exception handling.
     //  Consider refactoring to reduce duplicated code in handling methods.
-    @ExceptionHandler({NotFoundException.class, InsufficientFundsException.class, UserEmailAlreadyTakenException.class})
+    @ExceptionHandler({EntityNotFoundException.class, InsufficientFundsException.class, UserEmailAlreadyTakenException.class})
     public ResponseEntity<ErrorDetails> handleCustomExceptions(Exception e) {
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
@@ -34,7 +33,7 @@ public class ControllerExceptionHandler {
                 .getFieldErrors()
                 .stream()
                 .map(FieldError::getDefaultMessage)
-                .collect(Collectors.joining("\n"));
+                .collect(Collectors.joining(", "));
 
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
